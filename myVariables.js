f8e9ef99cee3fb29cdbcef4cd8e3a7d2faf65478
Ghadier-Alenezi <Ghadier.Mutar.elq.js@tuwaiq.edu.sa>
2021-10-14 11:50:25 +0300

    first task

---
+++ myVariables.js
@@ -0,0 +1,26 @@
+let language;
+language = "JavaScript";
+let score;
+score = 10;
+console.log(language + " " + score);
+
+let length;
+let width;
+let area;
+length = 30;
+width = 10;
+area = length * width;
+console.log(`The area of the rectangle is ${area}`);
+
+let num1;
+let num2;
+num1 = 9;
+num2 = 19;
+x = num2 % num1
+console.log(x);
+
+console.log(num1);
+
+x = num1/num2
+y = num1 % num2
+console.log(x + ` remainder ` + y);
---
+++ task2.js
@@ -0,0 +1,5 @@
+let length;
+let width;
+let area;
+area = length * width;
+console.log(`The area of the rectangle is ${area}`);
